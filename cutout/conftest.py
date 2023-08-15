3a7baf21f431fe780be702adb5e01b2e817cac03
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-08-15 00:06:38 +0000

    First Commit, Setup Django enviroment

---
+++ cutout/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cutout.users.models import User
+from cutout.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
